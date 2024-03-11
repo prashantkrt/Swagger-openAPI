194d5874555cce72b8c0d894c0cec289b83a8824
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-03-11 15:09:31 +0530

    Swagger 2 doesn't supported by Spring 3

---
+++ Spring-RestAPI-TicketBookingApplication-Producer_Swagger2/src/main/java/com/myLearning/springRest/swagger/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.myLearning.springRest.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket restApiDoc() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.myLearning.springRest.controller"))
+				.paths(PathSelectors.any()).build();
+	}
+
+}
